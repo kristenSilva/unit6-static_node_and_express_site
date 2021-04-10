29322efdef4feb5b202e926ce22197fe24c19ee6
kristenSilva <kristensilvarobles@gmail.com>
2021-04-09 21:15:35 -0700

    updating about and layout pug files

--- errorHandlers.js
+++ errorHandlers.js
@@ -1,12 +1,12 @@
 //Error handler for non-existent routes
 const doesNotExistError = (req, res, next) => {
     //Indicates that function is running
-    console.log('Handling 404 error');
+    console.log("Oops! Looks like the page you are looking for does not exist.");
     
     //Create custom Error Object
     const err = new Error();
     err.status = 404;
-    err.message = "Whoops! Looks like that route does not exist.";
+    err.message = "Oops! Looks like the page you are looking for does not exist.";
 
     res.status(404);
     res.render('page-not-found', {err});
@@ -16,13 +16,13 @@ const doesNotExistError = (req, res, next) => {
 //Global error handler
 const globalError = (err, req, res, next) => {
     //Indicates that function is running
-    console.log('Handling global error');
+    console.log("Oops! Looks like something went wrong on the server.");
     
     if(err.status === 404){
         res.status(404);
         res.render('page-not-found', {err});
     } else{
-        err.message = err.message || 'Whoops! Looks like something went wrong on the server.';
+        err.message = err.message || "Oops! Looks like something went wrong on the server.";
         res.status(err.status || 500);
         res.render('error', {err})
     }
